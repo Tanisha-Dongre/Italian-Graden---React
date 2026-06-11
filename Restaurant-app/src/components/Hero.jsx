0ff0bf7c1058816a0966310e03e5eb2f88038237
Tanisha Dongre <tanishadongre09@gmail.com>
2026-06-11 15:54:33 +0530

    Added hero section animation and card shadows

--- Restaurant-app/src/components/Hero.jsx
+++ Restaurant-app/src/components/Hero.jsx
@@ -1,11 +1,22 @@
 function Hero() {
   return (
     <section id="home" className="hero">
-      <h2>Authentic Italian Cuisine</h2>
+      <img
+        src="https://plus.unsplash.com/premium_photo-1664472619078-9db415ebef44?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGFzdGF8ZW58MHx8MHx8fDA%3D"
+        alt="Italian Food"
+      />
 
-      <p>Fresh Ingredients, handmade pasta, and classic Italian flavors.</p>
-      <a href="#menu">
-      <button>Order Now</button></a>
+      <div className="hero-content">
+        <h2>Authentic Italian Cuisine</h2>
+      
+        <p>
+          Fresh Ingredients, handmade pasta, and classic Italian flavors.
+        </p>
+      
+        <a href="#menu">
+          <button>Order Now</button>
+        </a>
+      </div>
     </section>
   );
 }
